fd7be4011c9db70e143a9e96d168a56041f13a27
Mkai Ey <139222639+mkaiey@users.noreply.github.com>
2023-12-05 21:04:31 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // Get rid of the CORS error
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://twitu-mkaiey.vercel.app",
         changeOrigin: true,
         secure: false,
       },
