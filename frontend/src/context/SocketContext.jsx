8191b0615010e4dc9c56890efc04dad6a088fe47
Mkai Ey <139222639+mkaiey@users.noreply.github.com>
2023-12-06 17:47:21 +0700

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   const user = useRecoilValue(userAtom);
 
   useEffect(() => {
-    const socket = io("http://localhost:5000", {
+    const socket = io("https://twitu-mkaiey.vercel.app", {
       query: {
         userId: user?._id,
       },
