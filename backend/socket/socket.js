9f7f2e5189f3aafe4c70be444c031db425ae3e21
Mkai Ey <139222639+mkaiey@users.noreply.github.com>
2023-12-06 17:42:53 +0700

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,8 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://twitu-app.vercel.app",
     methods: ["GET", "POST"],
+    credentials: true
   },
 });
 
