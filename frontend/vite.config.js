a537396e61b1901e84c0df802c40f2533acc57d3
Mkai Ey <139222639+mkaiey@users.noreply.github.com>
2023-12-06 17:52:56 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: "https://twitu-app.vercel.app",
     // Get rid of the CORS error
     proxy: {
       "/api": {
