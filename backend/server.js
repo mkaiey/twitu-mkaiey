6c101245cb435bf31ec58bebfbb3df3d9a482cc2
Mkai Ey <139222639+mkaiey@users.noreply.github.com>
2023-12-10 03:06:35 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,12 @@ cloudinary.config({
 });
 
 // Middlewares
-app.use(cors());
+app.use(
+	cors({
+		origin: ["https://twitu-app.vercel.app"],
+		credentials: true,
+	})
+)
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
